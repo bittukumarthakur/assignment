e0c6acff4bad2af6f33def3dec324b54fbe3f2af
Bittu <bittukumarthakur@users.noreply.github.com>
2023-04-12 12:51:25 +0530

    Add test is passing

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,20 @@
 const selectEvenNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isEven(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isEven(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
 const selectOddNumbers = function(numbers) {
-	const evenNumbers = [];
-	for(const number of numbers) {
-		if (isOdd(number)) {
-			evenNumbers.push(number);
-		}
-	}
-	return evenNumbers;
+  const evenNumbers = [];
+  for(const number of numbers) {
+    if (isOdd(number)) {
+      evenNumbers.push(number);
+    }
+  }
+  return evenNumbers;
 }
 
